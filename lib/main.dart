728f7856b03c949a893f2aecb56d4d87ddb46a33
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2021-12-16 22:05:59 +0530

    Dependency update Part 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:Chatify/constants.dart';
 import 'Screens/SplashScreen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
